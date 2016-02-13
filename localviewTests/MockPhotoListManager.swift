e763f3f6a67f05db7d0f77e8f299bb46890c9a34
Zach Freeman <zsfree00@gmail.com>
2016-02-13 17:58:44 -0500

    Add MockPhotoListManager to spec target

---
+++ localviewTests/MockPhotoListManager.swift
@@ -0,0 +1,17 @@
+//
+//  MockPhotoListManager.swift
+//  localview
+//
+//  Created by Zach Freeman on 2/7/16.
+//  Copyright © 2016 sparkwing. All rights reserved.
+//
+
+import Foundation
+
+class MockPhotoListManager : PhotoListManager {
+    var startNetworkStatusRequestCount = 0
+    override func startNetworkStatus() {
+        startNetworkStatusRequestCount = 1
+    }
+    
+}
