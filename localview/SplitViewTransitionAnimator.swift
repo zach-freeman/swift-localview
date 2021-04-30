6a3452eb01a0a2c2e8a73dc3239ef543776d6c3a
Zach Freeman <zsfree00@gmail.com>
2021-04-30 16:04:32 -0400

    Fix a few warnings

--- localview/SplitViewTransitionAnimator.swift
+++ localview/SplitViewTransitionAnimator.swift
@@ -19,35 +19,35 @@ class SplitViewTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioni
         let containerView = transitionContext.containerView
         let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
         let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
-        //get rects that represent the top and bottom halves of the screen
+        // get rects that represent the top and bottom halves of the screen
         let viewSize = fromViewController!.view.bounds.size
         let topFrame = CGRect(x: 0, y: 0, width: viewSize.width, height: viewSize.height/2)
         let bottomFrame = CGRect(x: 0, y: viewSize.height/2, width: viewSize.width, height: viewSize.height/2)
-        //create snapshots
+        // create snapshots
         let snapshotTop: UIView = fromViewController!.view
             .resizableSnapshotView(from: topFrame, afterScreenUpdates: false, withCapInsets: UIEdgeInsets.zero)!
         let snapshotBottom: UIView = fromViewController!.view
             .resizableSnapshotView(from: bottomFrame, afterScreenUpdates: false, withCapInsets: UIEdgeInsets.zero)!
         snapshotTop.frame = topFrame
         snapshotBottom.frame = bottomFrame
-        //remove the original view from the container
+        // remove the original view from the container
         fromViewController!.view.removeFromSuperview()
-        //add our destination view
+        // add our destination view
         containerView.addSubview(toViewController!.view)
-        //add our snapshots on top
+        // add our snapshots on top
         containerView.addSubview(snapshotTop)
         containerView.addSubview(snapshotBottom)
         UIView.animate(withDuration: duration, animations: {
-            //adjust the new frames
+            // adjust the new frames
             var newTopFrame:CGRect = topFrame
             var newBottomFrame:CGRect = bottomFrame
             newTopFrame.origin.y -= topFrame.size.height
             newBottomFrame.origin.y += bottomFrame.size.height
-            //set the frames to animate them
+            // set the frames to animate them
             snapshotTop.frame = newTopFrame
             snapshotBottom.frame = newBottomFrame
             }, completion: {_ in
-                //don't forget to clean up
+                // don't forget to clean up
                 snapshotTop.removeFromSuperview()
                 snapshotBottom.removeFromSuperview()
                 transitionContext.completeTransition(true)
